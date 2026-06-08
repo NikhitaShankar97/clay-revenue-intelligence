9e0e3b49de4cd76dcec12e15f2f7b493308ec090
Nikhita Shankar <nikhita.shankar97@gmail.com>
2026-06-08 04:41:01 -0700

    Create RAW.CUSTOMERS table in Snowflake

---
+++ snowflake/02_create_raw_tables.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE TABLE CLAY_REVENUE_INTELLIGENCE.RAW.CUSTOMERS (
+    customer_id INTEGER,
+    company_name STRING,
+    industry STRING,
+    plan_tier STRING,
+    created_at DATE
+);
